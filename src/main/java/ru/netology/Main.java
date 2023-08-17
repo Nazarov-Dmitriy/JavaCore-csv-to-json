38b663e1de3c61ae4543f3c25794c34e67fd8dd3
Nazarov Dmitry <Difat1@inbox.ru>
2023-08-17 13:30:35 +0300

    add working xml

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,21 +7,57 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
+        String fileNameXml = "data.xml";
 
         List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json);
+        List<Employee> listXml = parseXML(fileNameXml);
 
+        String json = listToJson(list);
+        String json2 = listToJson(listXml);
+
+        writeString(json, "new_data_csv.json");
+        writeString(json2, "new_data_xml.json");
+
+    }
+
+    private static List<Employee> parseXML(String fileNameXml) throws IOException, SAXException, ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileNameXml));
+        Node root = doc.getDocumentElement();
+        List<Employee> listXml = read(root);
+        return listXml;
+    }
+
+    private static List<Employee> read(Node node) {
+        NodeList nodeList = node.getChildNodes();
+        List<Employee> list = new ArrayList<Employee>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                String[] employ = node_.getTextContent().trim().replaceAll("\\s+", " ").split(" ");
+                list.add(new Employee(Long.parseLong(employ[0]), employ[1], employ[2], employ[3], Integer.parseInt(employ[4])));
+            }
+        }
+        return list;
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -49,9 +85,9 @@ public class Main {
         return json;
     }
 
-    private static void writeString(String json) {
+    private static void writeString(String json, String fileName) {
         try (FileWriter file = new
-                FileWriter("new_data.json")) {
+                FileWriter(fileName)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
